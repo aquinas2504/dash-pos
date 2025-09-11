28f95b2fa73590cdcd95451b14b15457e372938a
aquinas2504 <thomas2504ninjarebirth@gmail.com>
2025-09-11 11:49:34 +0700

    Merubah auth.login jadi Auth.login pada function showLogin()

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,7 +10,7 @@ class AuthController extends Controller
 {
     public function showLogin()
     {
-        return view('auth.login');
+        return view('Auth.login');
     }
 
     public function login(Request $request)
