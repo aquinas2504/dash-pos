a59bb42e9d87ad06ce74661e912e6aac3c46b504
aquinas2504 <thomas2504ninjarebirth@gmail.com>
2025-11-26 15:42:21 +0700

    ubah mode dev ke hosting index.php

--- public/index.php
+++ public/index.php
@@ -11,12 +11,12 @@ if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
 }
 
 // Register the Composer autoloader...
-// require __DIR__.'/vendor/autoload.php'; // Mode Hosting
-require __DIR__.'/../vendor/autoload.php'; // Mode Dev
+require __DIR__.'/vendor/autoload.php'; // Mode Hosting
+// require __DIR__.'/../vendor/autoload.php'; // Mode Dev
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
-// $app = require_once __DIR__.'/bootstrap/app.php'; // Mode Hosting
-$app = require_once __DIR__.'/../bootstrap/app.php'; // Mode Dev
+$app = require_once __DIR__.'/bootstrap/app.php'; // Mode Hosting
+// $app = require_once __DIR__.'/../bootstrap/app.php'; // Mode Dev
 
 $app->handleRequest(Request::capture());
